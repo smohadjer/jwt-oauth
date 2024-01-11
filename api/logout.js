958fc795c214c4b408257dc52ea169e948338dc0
Saeid Mohadjer <saeid@fastmail.fm>
2024-01-11 14:39:56 +0100

    bug cookie

--- api/logout.js
+++ api/logout.js
@@ -6,8 +6,12 @@ export default async (req, res) => {
 	console.log(req.cookies.refreshtoken);
 
 	if (req.cookies.refreshtoken) {
-		res.setHeader('Set-Cookie', ['refreshtoken=; expires=Thu, 01 Jan 1970 00:00:00 GMT HttpOnly; Path=/']);
-		res.setHeader('Set-Cookie', 'refreshtoken=deleted; Path=/; Max-Age=0');
+		res.setHeader('Set-Cookie', [
+			'refreshtoken=deleted; expires=Thu, 01 Jan 1970 00:00:00 GMT HttpOnly; Path=/',
+			'hello=world; HttpOnly; Path=/'
+		]);
+
+		//res.setHeader('Set-Cookie', 'refreshtoken=deleted; Path=/; Max-Age=0');
 
 		res.status(200).json({
 			message: 'refreshtoken cookie deleted'
