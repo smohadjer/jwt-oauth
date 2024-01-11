975e9bc6b6242cd7b9effbf45b5d36ffe81df349
Saeid Mohadjer <saeid@fastmail.fm>
2024-01-11 13:55:35 +0100

    minor

--- api/logout.js
+++ api/logout.js
@@ -3,8 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default async (req, res) => {
+	console.log(req.cookies.refreshtoken);
+
 	if (req.cookies.refreshtoken) {
-		res.setHeader('Set-Cookie', ['refreshtoken=deleted; expires=Thu, 01 Jan 1970 00:00:00 GMT HttpOnly; Path=/']);
+		res.setHeader('Set-Cookie', ['refreshtoken=; expires=Thu, 01 Jan 1970 00:00:00 GMT HttpOnly; Path=/']);
 
 		res.status(200).json({
 			message: 'refreshtoken cookie deleted'
