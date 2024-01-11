71472ec116432671dc2db940211a9cc9e500d565
Saeid Mohadjer <saeid@fastmail.fm>
2024-01-11 14:48:22 +0100

    test

--- api/logout.js
+++ api/logout.js
@@ -4,12 +4,16 @@ dotenv.config();
 
 export default async (req, res) => {
 	console.log(req.cookies.refreshtoken);
+	const secure = process.env.development ? '' : '; Secure';
 
 	if (req.cookies.refreshtoken) {
 		res.setHeader('Set-Cookie', [
 			'refreshtoken=deleted; expires=Thu, 01 Jan 1970 00:00:00 GMT HttpOnly; Path=/',
-			'hello=world; HttpOnly; Path=/'
+			`hello=world; HttpOnly; Path=/${secure}`
 		]);
+
+		//res.setHeader('Set-Cookie', [`refreshtoken=${refreshToken}; HttpOnly; Path=/${secure}`]);
+
 
 		//res.setHeader('Set-Cookie', 'refreshtoken=deleted; Path=/; Max-Age=0');
 
