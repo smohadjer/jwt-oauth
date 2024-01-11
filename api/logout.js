cb804bbfda1b9e0cf185da35f31e6a05721a0622
Saeid Mohadjer <saeid@fastmail.fm>
2024-01-11 15:44:09 +0100

    fix bug

--- api/logout.js
+++ api/logout.js
@@ -4,25 +4,10 @@ dotenv.config();
 
 export default async (req, res) => {
 	console.log(req.cookies.refreshtoken);
-	const secure = process.env.development ? '' : '; Secure';
 
-	if (req.cookies.refreshtoken) {
-		res.setHeader('Set-Cookie', [
-			'refreshtoken=deleted; expires=Thu, 01 Jan 1970 00:00:00 GMT HttpOnly; Path=/',
-			`hello=world; HttpOnly; Path=/${secure}`
-		]);
+	res.setHeader('Set-Cookie', ['refreshtoken=; expires=Thu, 01 Jan 1970 00:00:00 GMT HttpOnly; Path=/']);
 
-		//res.setHeader('Set-Cookie', [`refreshtoken=${refreshToken}; HttpOnly; Path=/${secure}`]);
-
-
-		//res.setHeader('Set-Cookie', 'refreshtoken=deleted; Path=/; Max-Age=0');
-
-		res.status(200).json({
-			message: 'refreshtoken cookie deleted'
-		});
-	} else {
-		res.status(200).json({
-			message: 'no refreshtoken cookie found'
-		});
-	}
+	res.status(200).json({
+		message: 'refreshtoken cookie deleted'
+	});
 }
